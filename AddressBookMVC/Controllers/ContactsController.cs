ceaaf91203a43bf8fbf262fcbb842df6b13477aa
arunpun <83875939+arunpun@users.noreply.github.com>
2021-10-24 19:02:22 +0100

    Added imageservice to all the views

--- AddressBookMVC/Controllers/ContactsController.cs
+++ AddressBookMVC/Controllers/ContactsController.cs
@@ -95,8 +95,14 @@ namespace AddressBookMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Address1,Address2,City,PostCode,Email,Phone,Created,ImageData,ImageType")] Contact contact)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Address1,Address2,City,PostCode,Email,Phone,Created,ImageData,ImageType,ImageFile")] Contact contact)
         {
+            if (contact.ImageFile != null)
+            {
+                contact.ImageData = await _imageService.ConvertFileToByteArrayAsync(contact.ImageFile);
+                contact.ImageType = contact.ImageFile.ContentType;
+            }
+
             if (id != contact.Id)
             {
                 return NotFound();
